fdcc0fb04a3bb90dd9ed22c00785d2a48ea62dc6
Shreya Swaroop <shreyaswrp@gmail.com>
2020-09-30 20:05:59 +0530

    feat: list all greetings in the database

--- main.js
+++ main.js
@@ -1,123 +1,103 @@
+const listButton = document.querySelector('.load-greetings');
 const postsLists = document.querySelector('.posts-list');
 const addPostForm = document.querySelector('.add-post-form');
+const editPostForm = document.querySelector('.edit-post-form');
 const url = 'http://localhost:4000/find-greetings';
 const urlForCreate = 'http://localhost:4000/create-greeting';
 const urlForEdit = 'http://localhost:4000/update-greeting';
-
-let firstnamevalue = document.getElementById('firstName-value');
-let lastnamevalue = document.getElementById('lastName-value');
-let greetingvalue = document.getElementById('greeting-value');
-
-let output ='';
-
-function getFirstName(val) {
-  firstnamevalue = val;
-}
-
-function getLastName(val) {
-  lastnamevalue = val;
-}
-
-function getGreeting(val) {
-  greetingvalue = val;
-}
+const urlForDelete = 'http://localhost:4000/delete-greeting';
 
 const renderPost = (posts) => {
-  /*posts.forEach(post =>{
+  let output = '';
+  posts.forEach(post =>{
     output += `<div class="card mt-4 col-md-3 bg-ligt" >
-    <div class="card-body" data-id=${post._id}>
-      <h6 class="card-title">${post._id}</h6>
-      <h6 class="card-title">${post.firstName +' ' +post.lastName}</h6>
-      <h6 class="card-title">${post.greeting}</h6>
-      <h6 class="card-title">${post.createdAt}</h6>
-      <a href="#" class="card-link" id="EditForm" onclick="editGreeting()">Edit</a>
-      <a href="#" class="card-link" id="DeleteForm" onclick="deleteGreeting()">Delete</a>
+    <div class="card-body">
+      <p class="ObjectId card-p">${post._id}</p>
+      <p class="NameOnCard card-p">${post.firstName +' ' +post.lastName}</p>
+      <p class="Greeting card-p">${post.greeting}</p>
+      <p mt -0 class="CreatedAt card-p">${post.createdAt}</p>
+     <div class="row edit-delete-button"> <div class=" list-group-item-action button-for-edit-delete col-md-5" id="EditForm" onclick="getFormToEditInPopup()">Edit</div>
+      <div class=" list-group-item-action button-for-edit-delete col-md-5" id="DeleteForm">Delete</div>
+    </div>
     </div>
   </div>`;
 });
-*/
-
-posts.map((post,index) =>{
-  output += `<div class="card mt-4 col-md-3 bg-ligt" >
-  <div class="card-body" data-id=${post._id}>
-    <h6 class="card-title">${post._id}</h6>
-    <h6 class="card-title">${post.firstName +' ' +post.lastName}</h6>
-    <h6 class="card-title">${post.greeting}</h6>
-    <h6 class="card-title">${post.createdAt}</h6>
-    <a href="#" class="card-link" id="EditForm" onclick="editGreeting()">Edit</a>
-    <a href="#" class="card-link" id="DeleteForm" onclick="deleteGreeting()">Delete</a>
-  </div>
-</div>`;
-});
-
 postsLists.innerHTML = output;
 }
 
-postsLists.addEventListener('click', (e) => {
-  console.log('9999');
-  e.preventDefault();
-  let varas = e.target.id == 'EditForm';
-  console.log(e.currentTarget.parentEl);
+//To load all the greetings
+function loadAllTheGreetings(){
+  fetch(url)
+  .then(res => res.json() )
+  .then(result => result.data)
+  .then(data => renderPost(data))
+}
+
+listButton.addEventListener('click', function() {
+  loadAllTheGreetings();
 })
 
-//Get: Read the greetings
-//Method : Get
+loadAllTheGreetings();
 
-fetch(url)
-.then(res => res.json() )
-.then(result => result.data)
-.then(data => renderPost(data));
+postsLists.addEventListener('click', (e) => {
+e.preventDefault();
+
+let deleteButtonPressed = e.target.id == 'DeleteForm'
+let editButtonPressed = e.target.id == 'EditForm'
+
+let id = e.target.parentElement.children[0].textContent;
+  
+  //Delete a greeting
+  //Method: delete
+  if(deleteButtonPressed){
+    fetch(`${urlForDelete}/${id}`,{
+
+      method:'DELETE',
+    })
+    .then(res => res.json())
+    .then(() => location.reload())
+  }
+})
 
 // Create -Insert new greeting
-// Method: POST
-
 function addGreetings(){
+  console.log('add function is called');
   fetch(urlForCreate, {
     method: 'POST',
     headers: {
     'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      firstName: firstnamevalue,
-      lastName: lastnamevalue,
-      greeting: greetingvalue 
+      firstName: document.querySelector('firstName'),
+      lastName: document.querySelector('lastName'),
+      greeting: document.querySelector('Greeting') 
     })
     }).then(data => {
-
-        closeForm();
-      
-      fetch(url)
-        .then(res => res.json() )
-        .then(result => result.data)
-        .then(data => renderPost(data));
+       closeForm();
+       fetch(url)
+      .then(res => res.json() )
+      .then(result => result.data)
+      .then(data => renderPost(data));
     }) 
     .catch(err => { 
       console.log(err);
     })
+    console.log('first name' +firstName);
 }
 
 function getFormInPopup() {
-  document.getElementById('addForm').style.display = "block";
+  document.querySelector('.add-post-greeting').style.display = "block";
+}
+
+function getFormToEditInPopup() {
+  document.querySelector('.edit-post-greeting').style.display = "block";
 }
 
 function closeForm() {
-  document.getElementById('addForm').style.display = "none";
+  document.querySelector('.add-post-greeting').style.display = "none";
 }
 
-//Edit a greeting
-//Method: Put
-
-function editGreeting(id) {
-console.log(document.getElementById('EditForm').getAttribute('data-id.id'));
-fetch(`${urlForEdit}/${id}`)
-.then()
-
+function closeFormForEdit() {
+  document.querySelector('.edit-post-greeting').style.display = "none";
 }
 
-//Delete a greeting
-//Method: Delete
-
-function deleteGreeting(id){
-  console.log('delete');
-
-}
